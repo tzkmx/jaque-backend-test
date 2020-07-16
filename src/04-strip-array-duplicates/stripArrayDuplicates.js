7dbe770a8d385ec0fd83dc04a162e4192b89e38a
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2020-07-16 14:13:47 -0500

    ejercicio 04 más documentado, enlazada implementación Set en CoffeeScript<ES6

--- src/04-strip-array-duplicates/stripArrayDuplicates.js
+++ src/04-strip-array-duplicates/stripArrayDuplicates.js
@@ -1,18 +1,23 @@
 /**
  * @module 04 - stripArrayDuplicates
- * @description Only works with array of scalar values,
- * composite (i.e. objects) should be iterated and
- * comparated with deeper equality first, but that is
- * an expensive algorithm, compare for example
- * {@link https://ramdajs.com/docs/#equals|Ramda} code for it.
+ * @description Incidentally works with any array of scalars
+ * like strings or booleans. It uses native ES6 Set object
+ * that naturally strips any duplicated element on construction
+ * or trying to add again items already in the set.
  */
 
 /**
- * Returns a new array stripping away scalar duplicated items.
+ * It uses native Set object, we could use a custom class or object as
+ * a map to keep register of found items and every new item being checked
+ * accessed at constant time "O(1)", therefore whole function runs at
+ * linear time "O(n)". See linked gist if need example of user land implementation.
  *
- * @see {@link https://wiki.developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/Set}
+ * @summary Returns a new array stripping away scalar duplicated items.
  *
- * @param {scalar[]} unorderedInput array with possibly duplicated items
+ * @see gist (YAGNI with ES6 Set): {@link https://gist.github.com/tzkmx/f6cab192b8615711605f|Custom dictionary Set before ES6}
+ * @see {@link https://wiki.developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/Set#Using_the_Set_object|Using the Set Object (MDN)}
+ *
+ * @param {number[]} unorderedInput array with possibly duplicated items
  * @return {Array} array without any duplicate
  */
 module.exports = function stripArrayDuplicates (unorderedInput) {
--- src/04-strip-array-duplicates/stripArrayDuplicates.test.js
+++ src/04-strip-array-duplicates/stripArrayDuplicates.test.js
@@ -6,6 +6,20 @@ describe('04. - stripArrayDuplicates', () => {
     expect(stripArrayDuplicates([])).toStrictEqual([])
   })
 
+  it('simple duplicates case', () => {
+    const justDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const expected = [...justDigits].reverse()
+
+    const input = expected.concat(justDigits).concat(justDigits)
+
+    expect(stripArrayDuplicates(input)).toStrictEqual(expected)
+  })
+
+  it('strips string of duplicates', () => {
+    expect(stripArrayDuplicates('firefox'))
+      .toStrictEqual('fireox'.split(''))
+  })
+
   it('returns array of digits from random string of numbers', () => {
     const expectedAfterOrder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -15,7 +29,8 @@ describe('04. - stripArrayDuplicates', () => {
       randomItemsBuild += Math.random()
     }
 
-    const inputArray = randomItemsBuild.split('.').join('').split('')
+    const inputArray = randomItemsBuild.split('.')
+      .join('').split('')
 
     const strippedDuplicates = stripArrayDuplicates(inputArray)
     strippedDuplicates.sort()
@@ -24,15 +39,23 @@ describe('04. - stripArrayDuplicates', () => {
   })
 })
 
-function buildTestHasAllWantedItems (items) {
+/**
+ * Builds a closure allowing builder test if every wanted item is included.
+ * @private
+ * @param {string|string[]|number[]} items to test include
+ * @param {boolean|undefined} verbose helps to see math to debug test
+ * @return {function} closure to reuse test rather than build the tests every time.
+ */
+function buildTestHasAllWantedItems (items, verbose) {
   return function hasAllWantedItems (tested) {
     let itemIndex
     for (itemIndex = 0; itemIndex < items.length; itemIndex++) {
       const lookFor = items[itemIndex].toString()
       if (!tested.includes(lookFor)) return false
     }
-
-    console.log(`Tested string: ${tested} has all wanted items: [${items.join(', ')}]`)
+    if (verbose) {
+      console.log(`Tested string: ${tested} has all wanted items: [${items.join(', ')}]`)
+    }
     return true
   }
 }
