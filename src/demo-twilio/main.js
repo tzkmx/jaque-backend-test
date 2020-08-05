88fdec00170f62ecf61bcf34b8a83a2866985c64
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2020-08-05 18:34:18 -0500

    generacion basica token chat grant c/express

--- src/demo-twilio/main.js
+++ src/demo-twilio/main.js
@@ -1,3 +1,25 @@
 require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
 
-console.log(process.env)
+const { generateToken } = require('./generateToken')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  const { identity } = req.body
+  const token = generateToken(identity)
+
+  res.json({
+    identity,
+    token: token.toJwt()
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
